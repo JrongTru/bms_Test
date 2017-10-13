ee28eb6fc41b3a9e0410ec734c208a959f9b77fb
jrongtru <jrongtru@outlook.com>
2017-10-13 18:40:20 +0800

    del files

--- cp.sh
+++
@@ -1,9 +0,0 @@
-#########################################################################
-# File Name: cp.sh
-# Author: TruJrong
-# mail: JrongTru@outlook.com
-# Created Time: Thu 28 Sep 2017 02:20:52 CST
-#########################################################################
-#!/bin/bash
-
-cp ./santak_c6_20ks.c ~/package/nut-2.7.2/drivers/santak_c6_20ks.c
